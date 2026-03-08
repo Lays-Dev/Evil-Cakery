69fdd743eb0ea8bd5176c086dcb387f94fb1174f
Jayden Currence <jaydencurrence@gmail.com>
2026-03-08 18:50:53 -0400

    Tweaks to Upgrade Class
    Also made a recording for the Lab Assignment Submission

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class upgradeButton
+{
+    [SerializeField] private Text upgradeText;
+    [SerializeField] private Button button;
+    
+}
+
+public class UIManager : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        
+
+    }
+
+    private void OnUpgradeButtonClicked()
+    {
+       
+    }
+}
